609dabb257e541be5eb8c044f910c01dd1cca82a
Eduardo Santos <3dmena@gmail.com>
2026-01-23 14:34:50 -0600

    feat: updated automatic clip board and ignoring files

---
+++ win10config/nvim/lua/plugins/clipboard.lua
@@ -0,0 +1,26 @@
+return {
+	"ojroques/nvim-osc52",
+	config = function()
+		require("osc52").setup({
+			max_length = 0,
+			silent = true,
+			trim = false,
+		})
+
+		local function copy(lines, _)
+			require("osc52").copy(table.concat(lines, "\n"))
+		end
+
+		local function paste()
+			return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+		end
+
+		vim.g.clipboard = {
+			name = "osc52",
+			copy = { ["+"] = copy, ["*"] = copy },
+			paste = { ["+"] = paste, ["*"] = paste },
+		}
+
+		vim.opt.clipboard = "aunnameplus"
+	end,
+}
